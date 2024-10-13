8a59dffe64b81a9d4ba6753d46fd8089628458f9
K-Zoldyck <devv-robert@outlook.com>
2024-10-13 14:49:14 -0300

    add: dto to countrie data

---
+++ countries_api/src/java/com/DTO/Countrie.java
@@ -0,0 +1,40 @@
+
+package com.DTO;
+public class Countrie {
+    private String g_name; // brazil
+    private String n_name; // Brazil
+    private String p_name; // Brasilia
+    private String region; // America,sul
+
+    public String getG_name() {
+        return g_name;
+    }
+
+    public void setG_name(String g_name) {
+        this.g_name = g_name;
+    }
+
+    public String getN_name() {
+        return n_name;
+    }
+
+    public void setN_name(String n_name) {
+        this.n_name = n_name;
+    }
+
+    public String getP_name() {
+        return p_name;
+    }
+
+    public void setP_name(String p_name) {
+        this.p_name = p_name;
+    }
+
+    public String getRegion() {
+        return region;
+    }
+
+    public void setRegion(String region) {
+        this.region = region;
+    }
+}
